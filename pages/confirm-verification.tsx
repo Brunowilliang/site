64054e6965c4d34c588073bf6633b30ef717c38a
Bruno Garcia <brunowilliang@icloud.com>
2023-02-13 19:16:17 -0300

    criando page de confirm-account

---
+++ pages/confirm-verification.tsx
@@ -0,0 +1,21 @@
+
+import React, { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
+import { api } from "../services/pocketbase";
+
+function PasswordReset() {
+
+  return (
+    <Center bg={'background'} h="100vh" mx='auto' flexDir="column">
+      <Text color={'white'} fontSize="2xl" fontWeight="bold" mb={4}>
+        Conta verificada com sucesso!
+      </Text>
+      <Button onClick={() => window.location.href = '/login'} width={'full'} size="lg" type="submit" colorScheme="blue" mt={4}>
+        Voltar
+      </Button>
+    </Center>
+  );
+}
+
+export default PasswordReset;
